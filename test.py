73686874c2b29d0d7c6da8dbbc0ea2987ee46fa9
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2011-12-27 20:40:51 -0200

    Added tests for strings.

---
+++ test.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*-
+
+import jsonfsm
+import unittest
+
+class TestString(unittest.TestCase):
+    def setUp(self):
+        self.python_string = u"• This is a test string."
+        self.json_string = '"' + self.python_string + '"'
+
+        self.another_python_string = u"Isto é uma string de teste."
+        self.another_json_string = '"' + self.another_python_string + '"'
+
+    def test_decode(self):
+        res = jsonfsm.loads(self.json_string)
+
+        self.assertTrue(isinstance(res, unicode))
+        self.assertEquals(res, self.python_string)
+
+    def test_surrounding_quotes(self):
+        # missing both "
+        self.assertRaises(jsonfsm.JSONParseError,
+                          jsonfsm.loads,
+                          self.json_string[1:][:-1])
+
+        # missing trailing "
+        self.assertRaises(jsonfsm.JSONParseError,
+                          jsonfsm.loads,
+                          self.json_string[1:])
+
+        # missing leading "
+        self.assertRaises(jsonfsm.JSONParseError,
+                          jsonfsm.loads,
+                          self.json_string[:-1])
+
+    def test_character_encoding(self):
+        l1_encoded = self.another_json_string.encode("latin1")
+        res = jsonfsm.loads(l1_encoded, "latin1")
+
+        self.assertEquals(res, jsonfsm.loads(self.another_json_string))
+        self.assertEquals(res, self.another_python_string)
+
+
+if __name__ == "__main__":
+    unittest.main()
